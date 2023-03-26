01bac65e7b855a3e3070abf2e6f7ee538821076d
kxowi <karim-xowi@live.fr>
2023-03-26 07:42:54 -0700

    update printf

---
+++ utils.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * is_printable - Evaluates if a char is printable
+ * @c: Char to be evaluated.
+ *
+ * Return: 1 if c is printable, 0 otherwise
+ */
+int is_printable(char c)
+{
+if (c >= 32 && c < 127)
+return (1);
+return (0);
+}
+/**
+ * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * @buffer: Array of chars.
+ * @i: Index at which to start appending.
+ * @ascii_code: ASSCI CODE.
+ * Return: Always 3
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
